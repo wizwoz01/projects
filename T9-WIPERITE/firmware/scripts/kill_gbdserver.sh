976e0458b540b264c8afb59c381e5bf593b82d00
wizwoz01 <ricardo.cervantes01@student.csulb.edu>
2026-01-03 23:49:22 -0800

    Delete T9-WIPERITE directory

--- T9-WIPERITE/firmware/scripts/kill_gbdserver.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-BOARD_USER="${BOARD_USER:-wiperite}"
-BOARD_HOST="${BOARD_HOST:-<BOARD-IP>}"
-BOARD_PORT="${BOARD_PORT:-22}"
-ssh -p "$BOARD_PORT" "$BOARD_USER@$BOARD_HOST" "killall gdbserver 2>/dev/null || true"
